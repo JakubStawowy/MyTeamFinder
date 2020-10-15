5819d717fe320602bc45c0784b02cd427e8a6dc3
Jakub <57110082+JakubStawowy@users.noreply.github.com>
2020-10-15 14:58:16 +0200

    Added docker configutation

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+            <title>Najlepsze filmy</title>
+            <link rel="stylesheet" href="style/style.css" type="text/css"/>
+            
+    </head>
+    <body>
+        
+    </body>
+</html>
